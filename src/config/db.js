83c19dadbac4e2b02d4b991ba706c11d5ae471b8
Hanumansai72 <hanumansai72@gmail.com>
2025-09-10 23:49:50 +0530

    cors fix5

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongooseURL);
+    await mongoose.connect("mongodb+srv://JUSTFAI:JUSTFAI123@cluster0.2diylnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("✅ Database Connected");
   } catch (err) {
     console.log("❌ Failed to connect database", err);
