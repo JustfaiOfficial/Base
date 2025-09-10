b3e8acb4212f7838d86e06cde85416abc0bce85c
Hanumansai72 <hanumansai72@gmail.com>
2025-09-10 23:57:20 +0530

    cors fix5

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
-// src/config/db.js
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://JUSTFAI:JUSTFAI123@cluster0.2diylnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("MONGO_URI=mongodb+srv://JUSTFAI:JUSTFAI123@cluster0.2diylnm.mongodb.net/test?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ Database Connected");
   } catch (err) {
     console.log("❌ Failed to connect database", err);
